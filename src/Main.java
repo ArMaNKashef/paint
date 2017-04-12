ce23602d0cd1a00b40cda2a889a36df62af0386a
Arman Kashef <armankzl71@gmail.com>
2017-04-12 10:48:38 +0430

    Merged

--- src/Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-	public static void main(String[] args) {
-		// ArrayList<Shape> list = new ArrayList<Shape>();
-		// list.add(new Rectangle(10,10,100,200,"blue","rectangle"));
-		// MyFrame frame = new MyFrame(list);
-		new LogIn();
-	}
-
-}
